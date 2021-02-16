8480ef21893cf967d54ac9e42ae773a3f6f8a170
PetarGuru <petarmulaj@gmail.com>
2021-02-16 12:18:40 +0100

    TileVania game
    Tile Vania game with all scripts.

---
+++ TileVania/Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class CoinPickup : MonoBehaviour {
+    [SerializeField] AudioClip coinPickUpSFX;
+    [SerializeField] int pointsForCoinPickup = 1;
+    private void OnTriggerEnter2D(Collider2D collision){
+        FindObjectOfType<GameSession>().AddToScore(pointsForCoinPickup);
+        AudioSource.PlayClipAtPoint(coinPickUpSFX, Camera.main.transform.position);
+        Destroy(gameObject);
+    }
+}
